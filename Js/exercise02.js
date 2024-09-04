c5c7ace327b894885b966d5ddcfafc8e09454e95
Phuwa-Oak <behemoth2535@gmail.com>
2024-09-04 11:48:13 +0700

    Add files via upload

---
+++ Js/exercise02.js
@@ -0,0 +1,2 @@
+const random_Number_ln_Range = (min,max)=>Math.random()*(max-min)+min;
+console.log(random_Number_ln_Range(2, 10));
---
+++ Js/scr.js
@@ -0,0 +1,5 @@
+console.log("hello javascript");
+console.log("hello สวัสดีวันพุธ");
+document.getElementById("text").innerHTML = "อ้วนนนนนนนน";
+document.write("Hello document write");
+window.alert("welcome to my page");/*เรียกใช้ id.text*/
